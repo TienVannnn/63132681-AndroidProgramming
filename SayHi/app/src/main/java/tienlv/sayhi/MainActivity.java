14e7e25ddf06c7fd0f6752a96ec8afe9662493e9
Tiến <letien3413@gmail.com>
2024-03-04 20:10:19 +0700

    Up App SayHi đơn giản

---
+++ SayHi/app/src/main/java/tienlv/sayhi/MainActivity.java
@@ -0,0 +1,21 @@
+package tienlv.sayhi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void HamXinChao(View v){
+        Toast.makeText(MainActivity.this, "Hello, Tôi tên là Lê Văn Tiến", Toast.LENGTH_SHORT).show();
+    }
+
+}
