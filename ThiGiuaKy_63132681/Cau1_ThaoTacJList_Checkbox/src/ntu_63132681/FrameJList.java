c6cae2d4f542fb26ab4fb742d06e0873f5d5908a
Tiến <letien3413@gmail.com>
2024-03-24 10:46:21 +0700

    Tạo khung layout

--- ThiGiuaKy_63132681/Cau1_ThaoTacJList_Checkbox/src/ntu_63132681/FrameJList.java
+++ ThiGiuaKy_63132681/Cau1_ThaoTacJList_Checkbox/src/ntu_63132681/FrameJList.java
@@ -5,6 +5,13 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.BorderLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.BoxLayout;
 
 public class FrameJList extends JFrame {
 
@@ -31,12 +38,61 @@ public class FrameJList extends JFrame {
 	 * Create the frame.
 	 */
 	public FrameJList() {
+		setTitle("Thao tác");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 651, 469);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.CENTER);
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[]{278, 339, 0};
+		gbl_panel.rowHeights = new int[]{65, 275, 71, 0};
+		gbl_panel.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+		panel.setLayout(gbl_panel);
+		
+		JPanel panel_1 = new JPanel();
+		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
+		gbc_panel_1.fill = GridBagConstraints.BOTH;
+		gbc_panel_1.insets = new Insets(0, 0, 5, 0);
+		gbc_panel_1.gridwidth = 2;
+		gbc_panel_1.gridx = 0;
+		gbc_panel_1.gridy = 0;
+		panel.add(panel_1, gbc_panel_1);
+		
+		JLabel lblNewLabel = new JLabel("Thao tác trên JList - Checkbox");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 30));
+		panel_1.add(lblNewLabel);
+		
+		JPanel panel_3 = new JPanel();
+		GridBagConstraints gbc_panel_3 = new GridBagConstraints();
+		gbc_panel_3.fill = GridBagConstraints.BOTH;
+		gbc_panel_3.insets = new Insets(0, 0, 5, 5);
+		gbc_panel_3.gridx = 0;
+		gbc_panel_3.gridy = 1;
+		panel.add(panel_3, gbc_panel_3);
+		panel_3.setLayout(new BoxLayout(panel_3, BoxLayout.X_AXIS));
+		
+		JPanel panel_4 = new JPanel();
+		panel_4.setLayout(null);
+		GridBagConstraints gbc_panel_4 = new GridBagConstraints();
+		gbc_panel_4.fill = GridBagConstraints.BOTH;
+		gbc_panel_4.insets = new Insets(0, 0, 5, 0);
+		gbc_panel_4.gridx = 1;
+		gbc_panel_4.gridy = 1;
+		panel.add(panel_4, gbc_panel_4);
+		
+		JPanel panel_2 = new JPanel();
+		GridBagConstraints gbc_panel_2 = new GridBagConstraints();
+		gbc_panel_2.fill = GridBagConstraints.BOTH;
+		gbc_panel_2.gridwidth = 2;
+		gbc_panel_2.gridx = 0;
+		gbc_panel_2.gridy = 2;
+		panel.add(panel_2, gbc_panel_2);
 	}
-
 }
