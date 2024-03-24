bef1456451a34b44d92829d8b9b07abb0495368c
Tiến <letien3413@gmail.com>
2024-03-24 15:28:14 +0700

    Tạo class Chương trình chính

---
+++ ThiGiuaKy_63132681/Cau1_ThaoTacJList_Checkbox/src/ntu_63132681/MainProgram.java
@@ -0,0 +1,10 @@
+package ntu_63132681;
+
+public class MainProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new FrameJList().setVisible(true);
+	}
+
+}
