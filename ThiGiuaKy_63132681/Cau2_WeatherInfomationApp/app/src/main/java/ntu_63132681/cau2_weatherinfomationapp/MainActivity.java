2e83e3046c1ded0221905a7ef13c0d6cd4ceca24
Tiến <letien3413@gmail.com>
2024-03-24 17:19:40 +0700

    setAdapter để hiển thị lên listview

--- ThiGiuaKy_63132681/Cau2_WeatherInfomationApp/app/src/main/java/ntu_63132681/cau2_weatherinfomationapp/MainActivity.java
+++ ThiGiuaKy_63132681/Cau2_WeatherInfomationApp/app/src/main/java/ntu_63132681/cau2_weatherinfomationapp/MainActivity.java
@@ -3,6 +3,9 @@ package ntu_63132681.cau2_weatherinfomationapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // tìm listview
+        ListView lvDs = findViewById(R.id.lvDS);
+        ArrayList<Weather> ds = new ArrayList<>();
+        ds.add(new Weather("Phú Yên", "Nắng gay gắt", "29°C", R.drawable.sunny));
+        ds.add(new Weather("Nha Trang", "Nắng có mây", "28°C", R.drawable.cloudy_sunny));
+        ds.add(new Weather("Tp Hồ Chí Minh", "Nắng gay gắt", "29°C", R.drawable.cloudy));
+        ds.add(new Weather("Hà Nội", "Mưa phùn", "17°C", R.drawable.rainy));
+        ds.add(new Weather("Bình Định", "Nắng có mây", "26°C", R.drawable.wind));
+        ds.add(new Weather("Sapa", "Lạnh có tuyết", "2°C", R.drawable.snowy));
+
+        WeatherAdapter adapterWeather = new WeatherAdapter(this, ds);
+        lvDs.setAdapter(adapterWeather);
     }
 }
