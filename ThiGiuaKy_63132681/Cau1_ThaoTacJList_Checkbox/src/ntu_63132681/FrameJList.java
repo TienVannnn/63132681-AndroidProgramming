1838c3386fb61f934af9d3424e5af5a66d6d950e
Tiến <letien3413@gmail.com>
2024-03-24 11:08:37 +0700

    Tạo khung layout lần 2

--- ThiGiuaKy_63132681/Cau1_ThaoTacJList_Checkbox/src/ntu_63132681/FrameJList.java
+++ ThiGiuaKy_63132681/Cau1_ThaoTacJList_Checkbox/src/ntu_63132681/FrameJList.java
@@ -12,6 +12,12 @@ import java.awt.Insets;
 import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.BoxLayout;
+import java.awt.Color;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.border.LineBorder;
+import javax.swing.border.TitledBorder;
 
 public class FrameJList extends JFrame {
 
@@ -40,7 +46,7 @@ public class FrameJList extends JFrame {
 	public FrameJList() {
 		setTitle("Thao tác");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 651, 469);
+		setBounds(100, 100, 773, 530);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
@@ -49,50 +55,66 @@ public class FrameJList extends JFrame {
 		
 		JPanel panel = new JPanel();
 		contentPane.add(panel, BorderLayout.CENTER);
-		GridBagLayout gbl_panel = new GridBagLayout();
-		gbl_panel.columnWidths = new int[]{278, 339, 0};
-		gbl_panel.rowHeights = new int[]{65, 275, 71, 0};
-		gbl_panel.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
-		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
-		panel.setLayout(gbl_panel);
+		panel.setLayout(null);
 		
 		JPanel panel_1 = new JPanel();
-		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
-		gbc_panel_1.fill = GridBagConstraints.BOTH;
-		gbc_panel_1.insets = new Insets(0, 0, 5, 0);
-		gbc_panel_1.gridwidth = 2;
-		gbc_panel_1.gridx = 0;
-		gbc_panel_1.gridy = 0;
-		panel.add(panel_1, gbc_panel_1);
+		panel_1.setBounds(0, 0, 748, 54);
+		panel.add(panel_1);
 		
 		JLabel lblNewLabel = new JLabel("Thao tác trên JList - Checkbox");
+		lblNewLabel.setForeground(new Color(0, 0, 255));
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 30));
 		panel_1.add(lblNewLabel);
 		
 		JPanel panel_3 = new JPanel();
-		GridBagConstraints gbc_panel_3 = new GridBagConstraints();
-		gbc_panel_3.fill = GridBagConstraints.BOTH;
-		gbc_panel_3.insets = new Insets(0, 0, 5, 5);
-		gbc_panel_3.gridx = 0;
-		gbc_panel_3.gridy = 1;
-		panel.add(panel_3, gbc_panel_3);
-		panel_3.setLayout(new BoxLayout(panel_3, BoxLayout.X_AXIS));
+		panel_3.setBorder(new TitledBorder(new LineBorder(new Color(60, 196, 73), 2, true), "Ch\u1ECDn thao t\u00E1c", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panel_3.setBounds(0, 64, 279, 347);
+		panel.add(panel_3);
+		panel_3.setLayout(null);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(22, 28, 85, 21);
+		panel_3.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_1.setBounds(22, 59, 85, 21);
+		panel_3.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("New button");
+		btnNewButton_2.setBounds(22, 100, 85, 21);
+		panel_3.add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("New button");
+		btnNewButton_3.setBounds(22, 139, 85, 21);
+		panel_3.add(btnNewButton_3);
+		
+		JButton btnNewButton_4 = new JButton("New button");
+		btnNewButton_4.setBounds(22, 181, 85, 21);
+		panel_3.add(btnNewButton_4);
+		
+		JButton btnNewButton_5 = new JButton("New button");
+		btnNewButton_5.setBounds(22, 222, 85, 21);
+		panel_3.add(btnNewButton_5);
 		
 		JPanel panel_4 = new JPanel();
+		panel_4.setBorder(new TitledBorder(new LineBorder(new Color(128, 0, 0), 2, true), "Nh\u1EADp th\u00F4ng tin", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panel_4.setBounds(278, 65, 470, 346);
 		panel_4.setLayout(null);
-		GridBagConstraints gbc_panel_4 = new GridBagConstraints();
-		gbc_panel_4.fill = GridBagConstraints.BOTH;
-		gbc_panel_4.insets = new Insets(0, 0, 5, 0);
-		gbc_panel_4.gridx = 1;
-		gbc_panel_4.gridy = 1;
-		panel.add(panel_4, gbc_panel_4);
+		panel.add(panel_4);
 		
 		JPanel panel_2 = new JPanel();
-		GridBagConstraints gbc_panel_2 = new GridBagConstraints();
-		gbc_panel_2.fill = GridBagConstraints.BOTH;
-		gbc_panel_2.gridwidth = 2;
-		gbc_panel_2.gridx = 0;
-		gbc_panel_2.gridy = 2;
-		panel.add(panel_2, gbc_panel_2);
+		panel_2.setBorder(new LineBorder(new Color(0, 0, 255), 2, true));
+		panel_2.setBounds(0, 415, 748, 67);
+		panel.add(panel_2);
+		panel_2.setLayout(null);
+		
+		JButton btnNewButton_6 = new JButton("Đóng chương trình");
+		btnNewButton_6.setBounds(239, 10, 267, 39);
+		btnNewButton_6.setFont(new Font("Tahoma", Font.BOLD, 22));
+		panel_2.add(btnNewButton_6);
 	}
 }
