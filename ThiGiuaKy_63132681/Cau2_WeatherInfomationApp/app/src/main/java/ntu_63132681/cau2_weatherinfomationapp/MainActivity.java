3962b9e4317cdc21e6d22ec7533c3ec59995a0b5
Tiến <letien3413@gmail.com>
2024-03-24 21:19:21 +0700

    Xử lý khi nhấn vào 1 item thì ra 1 layout khác để xem chi tiết item đó

--- ThiGiuaKy_63132681/Cau2_WeatherInfomationApp/app/src/main/java/ntu_63132681/cau2_weatherinfomationapp/MainActivity.java
+++ ThiGiuaKy_63132681/Cau2_WeatherInfomationApp/app/src/main/java/ntu_63132681/cau2_weatherinfomationapp/MainActivity.java
@@ -2,13 +2,18 @@ package ntu_63132681.cau2_weatherinfomationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    ArrayList<Weather> ds;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
         // tìm listview
         ListView lvDs = findViewById(R.id.lvDS);
-        ArrayList<Weather> ds = new ArrayList<>();
+        ds = new ArrayList<>();
         ds.add(new Weather("Phú Yên", "Nắng gay gắt", "29°C", R.drawable.sunny));
         ds.add(new Weather("Nha Trang", "Nắng có mây", "28°C", R.drawable.cloudy_sunny));
         ds.add(new Weather("Tp Hồ Chí Minh", "Nắng gay gắt", "29°C", R.drawable.cloudy));
@@ -26,5 +31,29 @@ public class MainActivity extends AppCompatActivity {
 
         WeatherAdapter adapterWeather = new WeatherAdapter(this, ds);
         lvDs.setAdapter(adapterWeather);
+
+        lvDs.setOnItemClickListener(ChuyenManHinh);
     }
+
+    AdapterView.OnItemClickListener ChuyenManHinh = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            Weather selected = ds.get(position);
+
+            Intent item_select = new Intent(MainActivity.this, WeatherDetail.class);
+
+            String tenTP = selected.getTenTP();
+            String moTa = selected.getMoTaThoiTiet();
+            String doC = selected.getDoC();
+            String img = String.valueOf(selected.getImage());
+
+            item_select.putExtra("tenTP", tenTP);
+            item_select.putExtra("moTa", moTa);
+            item_select.putExtra("do", doC);
+            item_select.putExtra("img", img);
+
+
+            startActivity(item_select);
+        }
+    };
 }
--- ThiGiuaKy_63132681/Cau2_WeatherInfomationApp/app/src/main/java/ntu_63132681/cau2_weatherinfomationapp/WeatherDetail.java
+++ ThiGiuaKy_63132681/Cau2_WeatherInfomationApp/app/src/main/java/ntu_63132681/cau2_weatherinfomationapp/WeatherDetail.java
@@ -2,13 +2,51 @@ package ntu_63132681.cau2_weatherinfomationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class WeatherDetail extends AppCompatActivity {
+    TextView tvTPDT, tvDoCDT, tvMoTaDT;
+    ImageView imgDT;
+    ImageButton btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather_detail);
+        init();
+
+        Intent item_detail = getIntent();
+
+        String tp = item_detail.getStringExtra("tenTP");
+        String moTa = item_detail.getStringExtra("moTa");
+        String doC = item_detail.getStringExtra("do");
+        String img = item_detail.getStringExtra("img");
+
+        tvTPDT.setText(tp);
+        tvMoTaDT.setText(moTa);
+        tvDoCDT.setText(doC);
+        imgDT.setImageResource(Integer.parseInt(img));
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WeatherDetail.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    void init(){
+        tvTPDT = findViewById(R.id.tvTPDT);
+        tvDoCDT = findViewById(R.id.tvDoCDT);
+        tvMoTaDT = findViewById(R.id.tvMoTaDT);
+        imgDT = findViewById(R.id.imgDT);
+        btnBack = findViewById(R.id.btnBack);
     }
 }
