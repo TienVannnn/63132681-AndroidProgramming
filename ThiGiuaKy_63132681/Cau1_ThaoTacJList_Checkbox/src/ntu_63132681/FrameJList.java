da79b80a8dfe234edaeaeac4244c4e393ae1ab83
Tiến <letien3413@gmail.com>
2024-03-24 09:54:42 +0700

    init project
    Thực hiện câu 1 với JList_Checkbox

---
+++ ThiGiuaKy_63132681/Cau1_ThaoTacJList_Checkbox/src/ntu_63132681/FrameJList.java
@@ -0,0 +1,42 @@
+package ntu_63132681;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class FrameJList extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FrameJList frame = new FrameJList();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public FrameJList() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+	}
+
+}
