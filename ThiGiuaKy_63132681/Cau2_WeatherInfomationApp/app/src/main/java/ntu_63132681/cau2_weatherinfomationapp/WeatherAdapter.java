05e2e58e6b514f4f9d0ebcfa38662e801f76ea99
Tiến <letien3413@gmail.com>
2024-03-24 16:59:12 +0700

    Viết hàm getView trong class WeatherAdapter để hiển thị dữ liệu cho mỗi item trong danh sách

--- ThiGiuaKy_63132681/Cau2_WeatherInfomationApp/app/src/main/java/ntu_63132681/cau2_weatherinfomationapp/WeatherAdapter.java
+++ ThiGiuaKy_63132681/Cau2_WeatherInfomationApp/app/src/main/java/ntu_63132681/cau2_weatherinfomationapp/WeatherAdapter.java
@@ -5,6 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -36,6 +38,25 @@ public class WeatherAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        if(convertView == null){
+            convertView = layoutInflater.inflate(R.layout.item_weather, null);
+        }
+
+        // lấy dữ liệu từ weather hiện tại
+        Weather current = ds.get(position);
+
+        // tìm điều khiển
+        TextView tvTenTp = convertView.findViewById(R.id.tvTenTP);
+        TextView tvMoTaTT = convertView.findViewById(R.id.tvMoTaThoiTiet);
+        TextView tvDoC = convertView.findViewById(R.id.tvDoC);
+        ImageView img = convertView.findViewById(R.id.imgThoiTiet);
+
+        // set lên giao diện
+        tvTenTp.setText(current.getTenTP());
+        tvMoTaTT.setText(current.getMoTaThoiTiet());
+        tvDoC.setText(current.getDoC());
+        img.setImageResource(current.getImage());
+
+        return convertView;
     }
 }
