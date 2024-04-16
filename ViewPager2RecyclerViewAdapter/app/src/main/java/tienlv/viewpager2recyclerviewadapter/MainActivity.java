e32dc10d74ab9bcf6924e26caa9b6b7816e55093
Tiến <letien3413@gmail.com>
2024-04-16 09:56:54 +0700

    add PageTransformer

--- ViewPager2RecyclerViewAdapter/app/src/main/java/tienlv/viewpager2recyclerviewadapter/MainActivity.java
+++ ViewPager2RecyclerViewAdapter/app/src/main/java/tienlv/viewpager2recyclerviewadapter/MainActivity.java
@@ -1,10 +1,12 @@
 package tienlv.viewpager2recyclerviewadapter;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.view.View;
 
 import java.util.ArrayList;
 
@@ -21,6 +23,32 @@ public class MainActivity extends AppCompatActivity {
         viewPager2Land = findViewById(R.id.vp2Land);
         landscapeAdapter = new LandscapeAdapter(this, ds);
         viewPager2Land.setAdapter(landscapeAdapter);
+//        viewPager2Land.setPageTransformer(new ViewPager2.PageTransformer() {
+//            @Override
+//            public void transformPage(@NonNull View page, float position) {
+//                final float MIN_SCALE = 0.75f;
+//                int pageWidth = page.getWidth();
+//                if (position < -1) {
+//                    page.setAlpha(0f);
+//                } else if (position <= 0) {
+//                    page.setAlpha(1f);
+//                    page.setTranslationX(0f);
+//                    page.setScaleX(1f);
+//                    page.setScaleY(1f);
+//                } else if (position <= 1) {
+//                    page.setAlpha(1 - position);
+//                    page.setTranslationX(pageWidth * -position);
+//                    float scaleFactor = MIN_SCALE
+//                            + (1 - MIN_SCALE) * (1 - Math.abs(position));
+//                    page.setScaleX(scaleFactor);
+//                    page.setScaleY(scaleFactor);
+//                } else {
+//                    page.setAlpha(0f);
+//                }
+//            }
+//        });
+
+        viewPager2Land.setPageTransformer(new ZoomOutPageTransformer());
     }
 
     ArrayList<Landscape> getDataViewPager2(){
@@ -32,3 +60,4 @@ public class MainActivity extends AppCompatActivity {
         return dsDuLieu;
     }
 }
+
