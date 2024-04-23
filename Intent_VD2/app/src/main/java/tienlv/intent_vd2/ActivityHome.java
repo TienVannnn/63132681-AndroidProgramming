c9c17d8bbe4487670c91b9ad469d39467076f3f2
Tiến <letien3413@gmail.com>
2024-04-23 20:06:11 +0700

    Xử lý sự kiện login và chuyển màn hình

--- Intent_VD2/app/src/main/java/tienlv/intent_vd2/ActivityHome.java
+++ Intent_VD2/app/src/main/java/tienlv/intent_vd2/ActivityHome.java
@@ -2,13 +2,19 @@ package tienlv.intent_vd2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ActivityHome extends AppCompatActivity {
+    TextView tvUserName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        tvUserName = findViewById(R.id.tvUserName);
+        Intent getIQuiz = getIntent();
+        tvUserName.setText(getIQuiz.getStringExtra("userName"));
     }
 }
--- Intent_VD2/app/src/main/java/tienlv/intent_vd2/ActivityLogin.java
+++ Intent_VD2/app/src/main/java/tienlv/intent_vd2/ActivityLogin.java
@@ -2,13 +2,52 @@ package tienlv.intent_vd2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ActivityLogin extends AppCompatActivity {
+    EditText userName, pass, mail;
+    Button btnOk;
+    private static final String EMAIL_REGEX =   "^[A-Za-z0-9]+[A-Za-z0-9]*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)$";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        init();
+        btnOk.setOnClickListener(ChuyenDenHome);
+    }
+
+    View.OnClickListener ChuyenDenHome = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String tenDN = userName.getText().toString();
+            String pw = pass.getText().toString();
+            String email = mail.getText().toString();
+            if(tenDN.equals("levantien") && pw.equals("63CNTT2")){
+                if(email.matches(EMAIL_REGEX)){
+                    Intent iQuiz = new Intent(ActivityLogin.this, ActivityHome.class);
+                    iQuiz.putExtra("userName", tenDN);
+                    startActivity(iQuiz);
+                }
+                else {
+                    Toast.makeText(ActivityLogin.this, "Email không hợp lệ!", Toast.LENGTH_SHORT).show();
+                }
+            }
+            else{
+                Toast.makeText(ActivityLogin.this, "Tên đăng nhập hoặc mật khẩu không đúng", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+
+    public void init(){
+        userName = findViewById(R.id.edtUserName);
+        pass = findViewById(R.id.edtPass);
+        mail = findViewById(R.id.edtMail);
+        btnOk = findViewById(R.id.btnOK);
     }
 }
