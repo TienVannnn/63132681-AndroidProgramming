03970e2df12dc1235ed46e6e90e2517d17c9f785
Tiến <letien3413@gmail.com>
2024-04-23 18:37:15 +0700

    Intent - Ví dụ 2
    Chuyển màn hình từ đăng nhập đến màn hình login

---
+++ Intent_VD2/app/src/main/java/tienlv/intent_vd2/ActivityLogin.java
@@ -0,0 +1,14 @@
+package tienlv.intent_vd2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
