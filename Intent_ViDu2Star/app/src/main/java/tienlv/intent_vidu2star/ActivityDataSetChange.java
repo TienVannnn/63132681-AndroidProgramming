fd2798d59848ac1e6c2849f6073fd66334436297
Tiến <letien3413@gmail.com>
2024-04-24 15:30:00 +0700

    done ví dụ 2 (nâng cao)
    xử lý sự kiện gọi activity khác và nhận kết quả trả về từ activity đó

--- Intent_ViDu2Star/app/src/main/java/tienlv/intent_vidu2star/ActivityDataSetChange.java
+++ Intent_ViDu2Star/app/src/main/java/tienlv/intent_vidu2star/ActivityDataSetChange.java
@@ -2,13 +2,56 @@ package tienlv.intent_vidu2star;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ActivityDataSetChange extends AppCompatActivity {
-
+    Button btnSetData, btnCancle;
+    EditText edtTen, edtNamSinh;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_set_change);
+        init();
+        btnSetData.setOnClickListener(setData);
+        btnCancle.setOnClickListener(cancle);
+
+    }
+
+    View.OnClickListener setData = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String ten = edtTen.getText().toString();
+            String nam = edtNamSinh.getText().toString();
+            if(ten.isEmpty() || nam.isEmpty()){
+                Toast.makeText(ActivityDataSetChange.this, "Chưa nhập dữ liệu!", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Intent iSet = new Intent();
+                iSet.putExtra("hoTen", ten);
+                iSet.putExtra("namSinh", nam);
+                setResult(RESULT_OK, iSet);
+                finish();
+            }
+        }
+    };
+
+    View.OnClickListener cancle = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            edtTen.setText("");
+            edtNamSinh.setText("");
+            edtTen.requestFocus();
+        }
+    };
+    public void init(){
+        btnSetData = findViewById(R.id.btnSetData);
+        btnCancle = findViewById(R.id.btnCancle);
+        edtTen = findViewById(R.id.edtHoTen);
+        edtNamSinh = findViewById(R.id.edtNamSinh);
     }
 }
--- Intent_ViDu2Star/app/src/main/java/tienlv/intent_vidu2star/MainActivity.java
+++ Intent_ViDu2Star/app/src/main/java/tienlv/intent_vidu2star/MainActivity.java
@@ -1,14 +1,39 @@
 package tienlv.intent_vidu2star;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tvHoten, tvNamsinh;
+    private ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        @Override
+        public void onActivityResult(ActivityResult o) {
+            if(o.getResultCode() == RESULT_OK){
+                Intent iDataNhanDuoc = o.getData();
+                tvHoten.setText(iDataNhanDuoc.getStringExtra("hoTen"));
+                tvNamsinh.setText(iDataNhanDuoc.getStringExtra("namSinh"));
+            }
+            else Toast.makeText(MainActivity.this, "Trả về thất bại", Toast.LENGTH_SHORT).show();
+        }
+    });
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvHoten = findViewById(R.id.tvHoTen);
+        tvNamsinh = findViewById(R.id.tvNamSinh);
+    }
+    public void HamMoManHinhNhapLieu(View v){
+        Intent iDataSetChange = new Intent(this, ActivityDataSetChange.class);
+        activityResultLauncher.launch(iDataSetChange);
     }
 }
