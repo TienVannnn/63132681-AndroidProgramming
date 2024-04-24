3b8e0e96d17d55928dbf2938683c66699259ec72
Tiến <letien3413@gmail.com>
2024-04-24 10:21:47 +0700

    Intent Ví dụ 2(Nâng cao) - Layout
    Thiết kế layout cho màn hình chính và màn hình nhập dữ liệu

---
+++ Intent_ViDu2Star/app/src/main/java/tienlv/intent_vidu2star/ActivityDataSetChange.java
@@ -0,0 +1,14 @@
+package tienlv.intent_vidu2star;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityDataSetChange extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_set_change);
+    }
+}
---
+++ Intent_ViDu2Star/app/src/main/java/tienlv/intent_vidu2star/MainActivity.java
@@ -0,0 +1,14 @@
+package tienlv.intent_vidu2star;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
